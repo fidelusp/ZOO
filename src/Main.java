2d8a4eab6ff6e014b0c535e349687d0e2b5319f7
Łukasz Ch <lukaszchajecki1@gmail.com>
2016-03-11 12:56:23 +0100

    dodałem Liste animals

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,26 @@
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        Dog dog = new Dog();
+        Animal dog = new Dog();
         dog.height = new BigDecimal(10);
 
-        Cat cat = new Cat();
+        Animal cat = new Cat();
         cat.length = new BigDecimal(5);
 
-        Owl owl = new Owl();
+        Animal owl = new Owl();
         owl.width = new BigDecimal(500);
 
-        System.out.println(dog.height);
+        List<Animal> animals = new ArrayList<>();
+        animals.add(dog);
+        animals.add(cat);
+        animals.add(owl);
+        System.err.println(animals.size());
+
+        System.out.println("Tutaj? " + animals.get(0).height);
         System.out.println(cat.length);
         System.out.print("Sowa " +owl.width);
 
