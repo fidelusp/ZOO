e5b18fb0c23e091764afedc676adbc9abe2a6819
Paweł Fidelus <fidelusp@gmail.com>
2016-03-11 12:09:08 +0100

    dodałem Doga

---
+++ src/Dog.java
@@ -0,0 +1,10 @@
+import java.math.BigDecimal;
+
+/**
+ * Created by Paweł on 2016-03-11.
+ */
+public class Dog {
+    BigDecimal length;
+    BigDecimal hight;
+    BigDecimal width;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import java.math.BigDecimal;
+
 /**
  * Created by Łukasz on 2016-03-11.
  */
 public class Main {
 
     public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.hight= new BigDecimal(10);
+
+        System.out.print(dog.hight);
 
     }
 }
